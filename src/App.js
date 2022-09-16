8ba34b4b65aa53c3ae0357b087b42a9d452107da
GitHub-Harrison <harrison-johnson@outlook.com>
2022-09-16 16:22:39 +0000

    finish todo list tutorial - local save not working

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import TodoList from './TodoList';
-import uuidv4 from 'uuid/v4';
+import { v4 as uuidv4 } from "uuid";
+
+const LOCAL_STORAGE_KEY = 'todoApp.todos'
 
 function App() {
   const [todos, setTodos] = useState([])
   const todoNameRef = useRef()
 
+  // to load todos from local storage
+  useEffect(() => {
+    const storedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY))
+    if (storedTodos) setTodos(storedTodos)
+  }, [])
+
+  // to save todos locally
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos))
+  }, [todos])
+
+  function toggleTodo(id) {
+    // create a copy of todos
+    const newTodos = [...todos]
+    const todo = newTodos.find(todo => todo.id === id)
+    todo.complete = !todo.complete
+    setTodos(newTodos)
+  }
+
+  // handle adding todos to the list
   function handleAddTodo(e) {
     const name = todoNameRef.current.value
     if (name === '') return
@@ -15,13 +37,20 @@ function App() {
     todoNameRef.current.value = null
   }
 
+  // clear completed todos
+  function handleClearTodos() {
+    const newTodos = todos.filter(todo => !todo.complete)
+    setTodos(newTodos)
+  }
+
+  // return the html of the page
   return (
     <>
-      <TodoList todos={todos} />
+      <TodoList todos={todos} toggleTodo={toggleTodo} />
       <input ref={todoNameRef} type="text" />
       <button onClick={handleAddTodo}>Add Todo</button>
-      <button>Clear Completed</button>
-      <div>0 left to do</div>
+      <button onClick={handleClearTodos}>Clear Complete</button>
+      <div>{todos.filter(todo => !todo.complete).length} left to do</div>
     </>
   );
 }
